82ac1b96439c1560e2f19dff1a89f1cc57e95cc3
koladev <onaelmangabo@gmail.com>
2024-02-04 18:52:18 +0100

    :sparkles: add authentication endpoints

--- ApiRoot/settings.py
+++ ApiRoot/settings.py
@@ -118,3 +118,15 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
+SIMPLE_JWT = {
+   'AUTH_HEADER_TYPES': ('JWT',),
+}
+
+
